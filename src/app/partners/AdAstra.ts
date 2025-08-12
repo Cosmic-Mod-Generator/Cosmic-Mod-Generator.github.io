8c76be51a23348214a12a9b25475f292a12254c0
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2025-08-12 12:30:29 -0400

    Typescript is so fussy

--- src/app/partners/AdAstra.ts
+++ src/app/partners/AdAstra.ts
@@ -1,26 +1,13 @@
 import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
-import { BooleanNode, ListNode, NumberNode, ObjectNode, Opt, Reference as RawReference, StringNode as RawStringNode } from '@mcschema/core'
+import { BooleanNode, ListNode, NumberNode, ObjectNode, Opt, StringNode as RawStringNode } from '@mcschema/core'
 
 const ID = 'ad_astra'
 
 export function initAdAstra(schemas: SchemaRegistry, collections: CollectionRegistry) {
-	const Reference = RawReference.bind(undefined, schemas)
 	const StringNode = RawStringNode.bind(undefined, collections)
 
 	collections.register('planets', ['earth', 'earth_orbit', 'glacio', 'glacio_orbit', 'mars', 'mars_orbit', 'mercury', 'mercury_orbit', 'moon', 'moon_orbit', 'venus', 'venus_orbit'])
 
-
-	// Not exported so we recreate it here
-	type NumberNodeConfig = {
-		/** Whether only integers are allowed */
-		integer?: boolean;
-		/** If specified, number will be capped at this minimum */
-		min?: number;
-		/** If specified, number will be capped at this maximum */
-		max?: number;
-		/** Whether the number represents a color */
-		color?: boolean;
-	};
 	
 	schemas.register(`${ID}:planets`, 
 		ObjectNode(
