5db93302faf8130f29db02c1bff4c7b2bf101eef
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2026-02-04 17:18:05 -0500

    Add typedoc

--- src/app/partners/AbstractPartner.ts
+++ src/app/partners/AbstractPartner.ts
@@ -3,6 +3,10 @@ import { CollectionRegistry, INode, Reference as RawReference, StringNode as Raw
 export abstract class AbstractPartner {
 	schemas: SchemaRegistry;
 	collections: CollectionRegistry;
+	
+	/**
+	 * A wrapper around {@link RawStringNode StringNode} that automatically uses this partners `collections`
+	 */
 	StringNode;
 
 	constructor(schemas: SchemaRegistry, collections: CollectionRegistry) {
@@ -16,13 +20,40 @@ export abstract class AbstractPartner {
 		this.initCollections(this.collections);
 	}
 
+	/**
+	 * Called to initialize schemas, aka generators for this partner.
+	 * @param schemas The schema registry to use for registering
+	 * @see {@link AbstractPartner.initCollections}
+	 */
 	abstract initSchemas(schemas: SchemaRegistry): void;
+
+	/**
+	 * Called to initialize collections, aka lists of presets for each generator.
+	 * @param collections The collection registry to use for registering
+	 * @see {@link AbstractPartner.initSchemas}
+	 */
 	abstract initCollections(collections: CollectionRegistry): void;
 
+	/**
+	 * Should return the namespace of this partners generators
+	 */
 	abstract getId(): string;
 
+	/**
+	 * Should return the full URL to fetch a .json file from for a preset.
+	 * 
+	 * For example, you may return something like ``https://raw.githubusercontent.com/User/Repo/main/resources/data/my_registry/${id}.json``
+	 * @param preset the preset id to fetch, a value from the `string[]` you would have registered in initCollections
+	 */
 	abstract mapPresetURL(preset: string): string;
 
+	/**
+	 * A wrapper around {@link RawReference Reference} that automatically uses this partners schema and id.
+	 * 
+	 * @param id The node id you want to reference
+	 * @param namespace Optional, defaults to this partners {@link AbstractPartner.getId()}. Specify this if you want to use a reference from another namespace
+	 * @returns The node
+	 */
 	Reference(id: string, namespace: string = this.getId()): INode {
 		return RawReference.bind(undefined, this.schemas).call(undefined, `${namespace}:${id}`)
 	}
